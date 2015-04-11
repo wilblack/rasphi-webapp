7c7b6355bc1101872252628dc4c040af96219952
Wil Black <wilblack21@gmail.com>
2015-04-11 12:47:08 -0700

    Update 2015-04-11T19:47:05.656Z

--- README.md
+++
@@ -1,1 +0,0 @@
-# rasphi-webapp
--- app/scripts/controllers/SettingsCtrl.js
+++
@@ -1,4 +0,0 @@
-angular.module('rasphiWebappApp')
-.controller('SettingsCtrl', function($rootScope, $scope, $ardyh, $sensorValues, ardyhConf, $localStorage, $user, $ionicLoading) {
-
-})
